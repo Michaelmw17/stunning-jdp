630102e929e1d96218d21e80a5d752636c584cc9
Michaelmw17 <Michaelmw17@outlook.com>
2021-04-12 02:16:58 +1000

    Updates

--- static/js/3.4d6bdaca.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{249:function(t,e,a){"use strict";a.r(e);var i=a(0),n=a(200),p=a.n(n),s=(a(219),a(1));e.default=function(){return Object(s.jsx)(i.Suspense,{fallback:Object(s.jsx)(p.a,{type:"Puff",color:"#00BFFF",height:100,width:100,timeout:3e3}),children:Object(s.jsx)("img",{src:"/stunning-jdp/JDPBRANDING-04.png",alt:"Logo",style:{height:"100px",width:"100px",display:"block",marginLeft:"auto",marginRight:"auto"},"max-age":"31536000"})})}}}]);
-//# sourceMappingURL=3.4d6bdaca.chunk.js.map
